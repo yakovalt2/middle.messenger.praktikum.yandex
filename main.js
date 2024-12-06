35739b9689c42c47a66437ff423646a39b853859
yakovalt2 <yakovalt2@gmail.com>
2024-12-06 16:21:20 +0300

    Fonts changes

---
+++ main.js
@@ -0,0 +1,8 @@
+import './src/styles/main.scss';
+import './src/styles/login.scss';
+import App from './src/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new App();
+  app.render();
+});
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { LoginPage } from "./loginPage";
+export { RegisterPage } from './registerPage'
