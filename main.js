576af578a5ae48e12ffd4d7cff44a8c594f2458a
yakovalt2 <yakovalt2@gmail.com>
2024-12-06 16:20:59 +0300

    Fonts added

--- main.js
+++
@@ -1,8 +0,0 @@
-import './src/styles/main.scss';
-import './src/styles/login.scss';
-import App from './src/App';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new App();
-  app.render();
-});
