c1556204181f6a5d6df44a1f3a6121c6f78d1e61
yakovalt2 <yakovalt2@gmail.com>
2024-12-10 01:25:55 +0300

    Navigation added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import Button from './components/Button.js';
 import Input from './components/Input.js';
 import Label from './components/Label.js'
 import Link from './components/Link.js';
+import Navigation from './components/Navigation.js'
+
 
 //Регистрация partials
 Handlebars.registerPartial('Button', Button);
 Handlebars.registerPartial('Input', Input);
 Handlebars.registerPartial('Label', Label)
 Handlebars.registerPartial('Link', Link);
+Handlebars.registerPartial('Navigation', Navigation);
 
 export default class App {
   constructor() {
@@ -36,21 +39,13 @@ export default class App {
   showPage(pageName) {
     let template;
 
-    console.log(Pages)
-
     const chats = [
       { id: 1, name: 'Андрей', lastMessage: 'Изображение', timestamp: '19:30' },
       { id: 2, name: 'Футбол', lastMessage: 'Вы: Играем сегодня?', timestamp: '17:10' },
       { id: 3, name: 'Илья', lastMessage: 'Друзья, у меня для вас особенный выпуск новостей!...', timestamp: '16:00' },
       { id: 4, name: 'Александр', lastMessage: 'Вы: Хорошо', timestamp: '12:17' },
-      { id: 4, name: 'Вадим', lastMessage: 'Привет', timestamp: 'вс' },
-      { id: 4, name: 'Илья', lastMessage: 'Вы: Да', timestamp: 'вс' },
-    ];
-
-    const menuItems = [
-      { href: "#account", text: "Аккаунт", icon: "/path/to/account-icon.png", className: "" },
-      { href: "#notifications", text: "Уведомления", icon: "/path/to/notifications-icon.png", className: "" },
-      { href: "#delete-account", text: "Удалить аккаунт", icon: "/path/to/delete-icon.png", className: "settings__menu-link--danger" },
+      { id: 5, name: 'Вадим', lastMessage: 'Привет', timestamp: 'вс' },
+      { id: 6, name: 'Илья', lastMessage: 'Вы: Да', timestamp: 'вс' },
     ];
 
     switch (pageName) {
@@ -62,7 +57,7 @@ export default class App {
         break;
       case 'settings':
         template = Handlebars.compile(Pages.SettingsPage);
-        this.appElement.innerHTML = template({
+        this.appElement.innerHTML = `${Navigation} ${template({
           avatar: "https://via.placeholder.com/100",
           first_name: "",
           second_name: "",
@@ -70,11 +65,11 @@ export default class App {
           login: "",
           email: "",
           phone: ""
-        })
+        })}`;
         return;
       case 'chats':
         template = Handlebars.compile(Pages.ChatsPage);
-        this.appElement.innerHTML = template({ chats });
+        this.appElement.innerHTML = `${Navigation} ${template({ chats })}`;
         return;
       case 'page404':
         document.addEventListener('click', (event) => {
@@ -99,7 +94,7 @@ export default class App {
         break;
     }
 
-    this.appElement.innerHTML = template({});
+    this.appElement.innerHTML = `${Navigation} ${template({})}`;
   }
 
 }
