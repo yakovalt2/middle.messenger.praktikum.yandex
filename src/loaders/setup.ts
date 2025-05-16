e33b70a547dae5dd2d08d0e193d5881df42e054f
yakovalt2 <yakovalt2@gmail.com>
2025-05-16 16:43:43 +0300

    Исправлены loaders

--- src/loaders/setup.ts
+++
@@ -1,8 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
-
-(globalThis as any).window = dom.window;
-(globalThis as any).document = dom.window.document;
-(globalThis as any).Node = dom.window.Node;
-(globalThis as any).HTMLElement = dom.window.HTMLElement;
--- src/loaders/stub-loader.ts
+++
@@ -1,9 +0,0 @@
-export async function load(url: string, context: any, defaultLoad: any) {
-  if (url.endsWith('.hbs') || url.endsWith('.scss')) {
-    return {
-      format: 'module',
-      source: 'export default {};',
-    };
-  }
-  return defaultLoad(url, context, defaultLoad);
-}
