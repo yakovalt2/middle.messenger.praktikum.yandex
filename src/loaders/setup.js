6d306f665e9f0cbee528ad6489e6ec558d6aee39
yakovalt2 <yakovalt2@gmail.com>
2025-05-16 17:38:05 +0300

    Fix

--- src/loaders/setup.js
+++ src/loaders/setup.js
@@ -1,4 +1,5 @@
 import { JSDOM } from "jsdom";
+import fetch, { Headers, Request, Response } from "node-fetch";
 
 const dom = new JSDOM("<!doctype html><html><body></body></html>");
 
@@ -6,3 +7,8 @@ globalThis.window = dom.window;
 globalThis.document = dom.window.document;
 globalThis.Node = dom.window.Node;
 globalThis.HTMLElement = dom.window.HTMLElement;
+
+globalThis.fetch = fetch;
+globalThis.Headers = Headers;
+globalThis.Request = Request;
+globalThis.Response = Response;
